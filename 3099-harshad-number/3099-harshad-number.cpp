783990b7d0eb9e8d3ef9addf481a9c70017e81cc
yosobanta <150269628+yosobanta@users.noreply.github.com>
2026-07-06 00:33:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.33%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int temp = x;
+        while(x>0){
+            sum+=x%10;
+            x/=10;
+        }    
+        return (temp%sum!=0)?-1:sum;
+    }
+};
